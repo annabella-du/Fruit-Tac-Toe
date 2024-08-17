70112d97a361bc67b012508a3d659824b1fb39b9
annabella-du <annabelladu1234@gmail.com>
2024-08-16 22:44:38 -0400

    Revert "redo"
    This reverts commit 90496c9fdfd0fa1de6749f8fa2f12c61781e4684.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,22 +1,12 @@
 extends Node2D
 
 @onready var global = get_node("/root/global")
-@onready var click_sfx: AudioStreamPlayer2D = $ClickSFX
-
-var next_scene : String
 
 func _on_instructions_button_pressed() -> void:
-	click_sfx.play()
-	
-	next_scene = "instructions"
+	global.load_scene("instructions")
 
 func _on_credits_button_pressed() -> void:
-	click_sfx.play()
-	next_scene = "credits"
+	global.load_scene("credits")
 
 func _on_play_button_pressed() -> void:
-	click_sfx.play()
-	next_scene = "select_icon"
-
-func _on_click_sfx_finished() -> void:
-	global.load_scene(next_scene)
+	global.load_scene("select_icon")
--- scripts/select_icon.gd
+++ scripts/select_icon.gd
@@ -12,6 +12,9 @@ var active_player := 1
 func _ready() -> void:
 	display_sprite.frame = 1
 
+func _physics_process(delta: float) -> void:
+	print(p1)
+
 func display_icon():
 	if active_player == 1:
 		player1_texture.texture = load(global.fruits[p1])
